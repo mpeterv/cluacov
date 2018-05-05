ca8e019e1f61b7f3f2c2d37f2728a741dc167cc5
Peter Melnichenko <mpeterval@gmail.com>
2018-05-05 16:29:12 +0300

    0.1.1 release

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.1.1 (2018-05-05)
+
+Updated dependency on luacov to 0.13.0.
+
+## 0.1.0 (2016-06-29)
+
+Initial release.
--- src/cluacov/version.lua
+++ src/cluacov/version.lua
@@ -3,4 +3,4 @@
 -- in C and may easily error on load due to a missing symbol,
 -- for instance, leaving the caller of `require` thinking that
 -- they are missing.
-return "0.1.0"
+return "0.1.1"
